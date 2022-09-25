592c012b7fdd7de11e32024d886bc7664b52ba60
荣兵强 <1932884599@qq.com>
2022-09-25 17:16:44 +0800

    order80

--- cloud-consumer-order80/src/main/java/com/rong/springcloud/order80.java
+++ cloud-consumer-order80/src/main/java/com/rong/springcloud/order80.java
@@ -2,7 +2,12 @@ package com.rong.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 /**
  * @title: PaymentMain8001
@@ -12,6 +17,8 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @date 2022/4/216:38
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableScheduling
 public class order80 {
     public static void main(String[] args) {
         ConfigurableApplicationContext test = SpringApplication.run(order80.class,args);
